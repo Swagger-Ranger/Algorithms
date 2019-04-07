e38f8f95b4c511ed7809e3e0a2e761b5b01a7513
Swagger-Ranger <liufei32@outlook.com>
2019-04-07 12:27:10 +0800

    提交代码示例     1.注解     2.反射

--- src/JavaCore/Enum/Enum_Advance_Singleton.java
+++ src/JavaCore/Enum/Enum_Advance_Singleton.java
@@ -8,7 +8,8 @@ package JavaCore.Enum;
  *         在枚举类型的序列化和反序列化上，Java做了特殊的规定：在序列化时Java仅仅是将枚举对象的name属性输出到结果中，
  *         反序列化的时候则是通过java.lang.Enum的valueOf方法来根据名字查找枚举对象。编译器是不允许任何对这种序列化机制的定制的
  *         并禁用了writeObject、readObject、readObjectNoData、writeReplace和readResolve等方法，从而保证了枚举实例的唯一性
- * 反射：反射无法创建枚举的实例，枚举的实例只能由编译器创造
+ * 反射：反射无法创建枚举的实例，枚举的实例只能由编译器创造,在使用reflect 包中Constructor新建实例时源码中就明确判断是否为枚举类型，
+ *       如果是枚举类型则无法新建实例
  *
  * 但缺点就是，枚举时占用的内存常常是静态变量的两倍还多
  */
